7e6eb9c12d62dfc488b777b162ccb59354ae0ff4
daltonPinaculo <dalton@pinaculodigital.com.br>
2026-03-07 12:33:23 -0300

    feat:  estrutura básica do lexer e palavras reservadas

--- lexer.py
+++ lexer.py
@@ -1,11 +1,27 @@
 from globals import TokenType, Token
 import globals
-
 class Lexer:
     def __init__(self, code):
         self.code = code
         self.pos = 0
         self.line = 1
 
-    
-    
+        self.keywords = {
+            "if": TokenType.IF,
+            "else": TokenType.ELSE,
+            "while": TokenType.WHILE,
+            "return": TokenType.RETURN,
+            "def": TokenType.DEF,
+            "print": TokenType.PRINT,
+            "var": TokenType.VAR,
+            "set": TokenType.SET,
+            "int": TokenType.INT,
+            "real": TokenType.REAL,
+            "bool": TokenType.BOOL,
+            "void": TokenType.VOID,
+            "true": TokenType.TRUE,
+            "false": TokenType.FALSE,
+            "and": TokenType.AND,
+            "or": TokenType.OR,
+            "not": TokenType.NOT
+        }
