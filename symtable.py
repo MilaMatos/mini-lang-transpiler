e316fe766e306b21d5d27c18827cbc9b392c1be1
pedro-rafaiel <pedrorafaelpereiradeoliveira@gmail.com>
2026-03-13 15:58:22 -0300

    feat: adiciona travessia da AST e controle de escopo no analisador semântico

--- symtable.py
+++ symtable.py
@@ -1,6 +1,7 @@
 from globals import NodeKind, StmtKind, ExpKind, TokenType
 from error import semantic_error
 
+
 class SymbolTable:
     def __init__(self):
         self.scopes = [{}]
@@ -22,3 +23,67 @@ class SymbolTable:
             if name in s:
                 return s[name]
         semantic_error(line, f"Variável '{name}' não declarada.")
+
+
+class SemanticAnalyzer:
+    def __init__(self):
+        self.symtab = SymbolTable()
+
+    def analyze(self, tree):
+        self.traverse(tree)
+
+    def traverse(self, t):
+        while t is not None:
+            self.pre_visit(t)
+
+            for i in range(3):
+                if t.child[i]:
+                    self.traverse(t.child[i])
+
+            self.post_visit(t)
+            t = t.sibling
+
+    def pre_visit(self, t):
+        if t.nodekind == NodeKind.STMT:
+
+            if t.kind == StmtKind.BLOCKK:
+                self.symtab.push()
+
+            elif t.kind == StmtKind.DEFK:
+                params = []
+                p = t.child[0]
+
+                while p:
+                    params.append(p.type)
+                    p = p.sibling
+
+                self.symtab.funcs[t.attr] = {
+                    'ret': t.type,
+                    'params': params
+                }
+
+                self.symtab.push()
+
+            elif t.kind == StmtKind.PARAMK:
+                self.symtab.declare(t.attr, t.type, t.lineno)
+
+    def post_visit(self, t):
+        if t.nodekind == NodeKind.STMT:
+
+            if t.kind in (StmtKind.BLOCKK, StmtKind.DEFK):
+                self.symtab.pop()
+
+            elif t.kind == StmtKind.VARDK:
+                self.symtab.declare(t.attr, t.type, t.lineno)
+
+        elif t.nodekind == NodeKind.EXP:
+
+            if t.kind == ExpKind.IDK:
+                t.type = self.symtab.lookup(t.attr, t.lineno)
+
+            elif t.kind == ExpKind.CALLK:
+                if t.attr not in self.symtab.funcs:
+                    semantic_error(t.lineno, f"Função '{t.attr}' não declarada.")
+
+                f_info = self.symtab.funcs[t.attr]
+                t.type = f_info['ret']
