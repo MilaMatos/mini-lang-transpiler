66fe71b7380537227bda1d8d8f2fca3baf84f79f
pedro-rafaiel <pedrorafaelpereiradeoliveira@gmail.com>
2026-03-13 16:07:22 -0300

    feat: adiciona tratamento de erros semânticos no módulo de erros

--- error.py
+++ error.py
@@ -9,3 +9,7 @@ class SyntaxErrors:
 def print_error(msg):
     print(f"Erro Sintático na linha {globals.lineno}: {msg}")
     exit(1)
+
+def semantic_error(linha, msg):
+    print(f"Erro Semântico na linha {linha}: {msg}")
+    exit(1)
